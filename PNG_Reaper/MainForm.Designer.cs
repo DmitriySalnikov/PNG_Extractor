1d9e21ca48f57f579c6407adc9f2db5eb8b9df58
DimOkGamer <salnikov.mine@yandex.ru>
2017-11-18 07:31:00 +0300

    Init

---
+++ PNG_Reaper/MainForm.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace PNG_Reaper
+{
+	partial class PNG_Reaper
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.components = new System.ComponentModel.Container();
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(PNG_Reaper));
+			this.b_reap = new System.Windows.Forms.Button();
+			this.l_file = new System.Windows.Forms.Label();
+			this.b_select = new System.Windows.Forms.Button();
+			this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+			this.label1 = new System.Windows.Forms.Label();
+			this.tb_chunck = new System.Windows.Forms.TextBox();
+			this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+			this.SuspendLayout();
+			// 
+			// b_reap
+			// 
+			resources.ApplyResources(this.b_reap, "b_reap");
+			this.b_reap.Name = "b_reap";
+			this.toolTip1.SetToolTip(this.b_reap, resources.GetString("b_reap.ToolTip"));
+			this.b_reap.UseVisualStyleBackColor = true;
+			this.b_reap.Click += new System.EventHandler(this.b_reap_Click);
+			// 
+			// l_file
+			// 
+			resources.ApplyResources(this.l_file, "l_file");
+			this.l_file.Name = "l_file";
+			this.toolTip1.SetToolTip(this.l_file, resources.GetString("l_file.ToolTip"));
+			// 
+			// b_select
+			// 
+			resources.ApplyResources(this.b_select, "b_select");
+			this.b_select.Name = "b_select";
+			this.toolTip1.SetToolTip(this.b_select, resources.GetString("b_select.ToolTip"));
+			this.b_select.UseVisualStyleBackColor = true;
+			this.b_select.Click += new System.EventHandler(this.b_select_Click);
+			// 
+			// openFileDialog1
+			// 
+			this.openFileDialog1.FileName = "somefile";
+			resources.ApplyResources(this.openFileDialog1, "openFileDialog1");
+			// 
+			// label1
+			// 
+			resources.ApplyResources(this.label1, "label1");
+			this.label1.Name = "label1";
+			this.toolTip1.SetToolTip(this.label1, resources.GetString("label1.ToolTip"));
+			// 
+			// tb_chunck
+			// 
+			resources.ApplyResources(this.tb_chunck, "tb_chunck");
+			this.tb_chunck.Name = "tb_chunck";
+			this.toolTip1.SetToolTip(this.tb_chunck, resources.GetString("tb_chunck.ToolTip"));
+			// 
+			// toolTip1
+			// 
+			this.toolTip1.ToolTipTitle = "Размер блока считываемых данных. Может повлиять на производительность.";
+			// 
+			// PNG_Reaper
+			// 
+			resources.ApplyResources(this, "$this");
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.Controls.Add(this.tb_chunck);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.b_select);
+			this.Controls.Add(this.l_file);
+			this.Controls.Add(this.b_reap);
+			this.Name = "PNG_Reaper";
+			this.toolTip1.SetToolTip(this, resources.GetString("$this.ToolTip"));
+			this.Load += new System.EventHandler(this.PNG_Reaper_Load);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button b_reap;
+		private System.Windows.Forms.Label l_file;
+		private System.Windows.Forms.Button b_select;
+		private System.Windows.Forms.OpenFileDialog openFileDialog1;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.TextBox tb_chunck;
+		private System.Windows.Forms.ToolTip toolTip1;
+	}
+}
+
---
+++ PNG_Reaper/MainForm.cs
@@ -0,0 +1,196 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PNG_Reaper
+{
+	public partial class PNG_Reaper : Form
+	{
+		int ArraySize = 50000;
+		int MAX_ARRAY_SIZE = 100000000;
+		int MIN_ARRAY_SIZE = 100;
+
+		byte[] PNG_Start = new byte[] { 0x89, 0x50, 0x4e, 0x47 };
+		byte[] PNG_End = new byte[] { 0x49, 0x45, 0x4e, 0x44, 0xae, 0x42, 0x60, 0x82 };
+
+		public PNG_Reaper()
+		{
+			InitializeComponent();
+		}
+
+		private void b_select_Click(object sender, EventArgs e)
+		{
+			if (openFileDialog1.ShowDialog() == DialogResult.OK)
+			{
+				l_file.Text = openFileDialog1.FileName;
+			}
+		}
+
+		private int find_array(ref byte[] arr, ref byte[] template, int offset, bool for_end = false)
+		{
+			bool found = false;
+			int res = -1;
+			if (arr.Length >= template.Length)
+			{
+				for (int i = offset; i <= arr.Length - template.Length; i++)
+				{
+					if (arr[i] == template[0])
+					{
+						for (int j = 1; j < template.Length; j++)
+						{
+							if (arr[i + j] != template[j])
+								break;
+							if (j == template.Length - 1)
+							{
+								if (!for_end)
+									res = i;
+								else
+									res = i + j + 1;
+								found = true;
+								break;
+							}
+						}
+					}
+					if (found)
+						break;
+				}
+			}
+			return res;
+		}
+
+		private void b_reap_Click(object sender, EventArgs e)
+		{
+			parce_chunk_value();
+
+			String path = openFileDialog1.FileName;
+
+			if (path != "" && !File.Exists(path))
+			{
+				return;
+			}
+			String file_name = path.Split('\\').Last();
+			String folder = path.Replace(file_name, "") + "PNGs_from_" + file_name + "\\";
+
+			BinaryReader reader = new BinaryReader(File.OpenRead(path));
+			Stream stream = reader.BaseStream;
+			BinaryWriter writer;
+
+			List<long> starts = new List<long>();
+			List<long> ends = new List<long>();
+
+			while (stream.Position < stream.Length)
+			{
+				long to_end = stream.Length - stream.Position;
+				long old_pos = stream.Position;
+				int size = to_end < ArraySize ? (int)to_end : (ArraySize > stream.Length) ? (int)stream.Length : ArraySize;
+
+				byte[] arr = reader.ReadBytes(size);
+				int offset_start = 0;
+				int offset_end = 0;
+
+				while (offset_start != -1 || offset_end != -1)
+				{
+					int pos;
+					if (offset_start != -1)
+					{
+						pos = find_array(ref arr, ref PNG_Start, offset_start);
+						offset_start = pos;
+						if (pos != -1)
+						{
+							offset_start += 4; //PREVENTING INFINITE LOOP
+
+							long new_position = old_pos + pos;
+							if (!starts.Contains(new_position))
+								starts.Add(new_position);
+						}
+					}
+
+					if (offset_end != -1)
+					{
+						pos = find_array(ref arr, ref PNG_End, offset_end, true);
+						offset_end = pos;
+						if (pos != -1)
+						{
+							long new_position = old_pos + pos;
+							if (!ends.Contains(new_position))
+								ends.Add(new_position);
+						}
+					}
+				}
+
+				if (size > 8)
+				{
+					stream.Seek(-8, SeekOrigin.Current);
+				}
+			}
+
+			int first = starts.Count;
+			int second = ends.Count;
+			if (second >= first)
+				second = first; //for get something if part of data corrupted
+			else
+				first = 0;
+
+			if (first > 0)
+			{
+				for (int i = 0; i < first; i++)
+				{
+					Directory.CreateDirectory(folder);
+					String new_file_name = folder + i.ToString() + ".png";
+					if (File.Exists(new_file_name))
+						File.Delete(new_file_name);
+
+					writer = new BinaryWriter(File.OpenWrite(folder + i.ToString() + ".png"));
+
+					stream.Seek(starts[i], SeekOrigin.Begin);
+					long size = ends[i] - starts[i];
+
+					while (size > 0)
+					{
+						if (size > int.MaxValue)
+						{
+							size -= int.MaxValue;
+							writer.Write(reader.ReadBytes(int.MaxValue));
+						}
+						else
+						{
+							writer.Write(reader.ReadBytes((int)size));
+							size = 0;
+						}
+					}
+					writer.Close();
+				}
+				MessageBox.Show("Exported PNGs: " + first.ToString(), "Done!");
+				reader.Close();
+			}
+			else
+				MessageBox.Show("Nothing found!", "Done!");
+
+			reader.Close();
+		}
+
+		private void PNG_Reaper_Load(object sender, EventArgs e)
+		{
+			tb_chunck.Text = ArraySize.ToString();
+		}
+
+		private void parce_chunk_value()
+		{
+			Int32.TryParse(tb_chunck.Text, out int res);
+			ArraySize = res;
+			if (res > MAX_ARRAY_SIZE)
+				res = MAX_ARRAY_SIZE;
+			if (res < MIN_ARRAY_SIZE)
+				res = MIN_ARRAY_SIZE;
+
+			tb_chunck.Text = res.ToString();
+		}
+	}
+}
